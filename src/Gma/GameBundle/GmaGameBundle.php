719a188914e3774b4f91a69b5c886863804e2ee9
lvazquez <lvazquez@ebowe.com>
2013-10-11 20:41:30 -0300

    Base del proyecto (Symfony 2.3.6)

---
+++ src/Gma/GameBundle/GmaGameBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gma\GameBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GmaGameBundle extends Bundle
+{
+}
---
+++ src/Gma/SiteBundle/GmaSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gma\SiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GmaSiteBundle extends Bundle
+{
+}
