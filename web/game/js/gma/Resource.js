eeb266b2a271a3adf5a8333e2b390ee401d9d31e
Diego Perez Barreiro <dperez@ebowe.com>
2013-10-13 01:47:40 -0300

    resources

--- web/game/js/gma/Resource.js
+++ web/game/js/gma/Resource.js
@@ -1,7 +1,5 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: dperez
- * Date: 10/13/13
- * Time: 12:00 AM
- * To change this template use File | Settings | File Templates.
- */
+define([], function () {
+    return new function () {
+        this.loader = null;
+    }
+});
