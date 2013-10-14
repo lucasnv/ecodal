a3fce7e8f4918f8fe4d9602e58e9e9beea9d7549
Diego Perez Barreiro <dperez@ebowe.com>
2013-10-13 22:32:16 -0300

    entity

--- web/game/js/gma/Entity.js
+++ web/game/js/gma/Entity.js
@@ -1,48 +1,119 @@
-define(['myclass'], 
+define(['myclass'],
     function (my) {
-    "use strict";
+        "use strict";
 
-    return my.Class({
+        return my.Class({
 
-        constructor: function (stage) {
-            /* Attr*/
-            this.parent = null;
-            this.body = null;
-            this.children = [];
-           /* this.x = 0;
-            this.y = 0;
-            this.z = 0;*/
-            this.stage = stage;
-        },
+            constructor: function (stage) {
+                /* Attr*/
+                this.parent = null;
+                this.body = null;
+                this.children = [];
+                /* this.x = 0;
+                 this.y = 0;
+                 this.z = 0;*/
+                this.stage = stage;
 
-      /*  setPosition: function(x, y ,z){
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        },*/
+                this.state = {
+                    gesture: null
+                };
 
-        addChild: function (child) {
-            child.parent = this;
-            child.embody();
-            this.children.push(child);
-        },
+                this.sprites = {};
+                this.gestures = {};
+            },
 
-        delParent: function(child){
-            child.parent = null;
-        },
+            /*  setPosition: function(x, y ,z){
+             this.x = x;
+             this.y = y;
+             this.z = z;
+             },*/
 
-        //NO LA ENTIENDO
-        embody: function () {
-            if (this.stage && this.body) {
-                this.stage.addChild(this.body);
-                this.render();
+            addChild: function (child) {
+                child.parent = this;
+                child.embody();
+                this.children.push(child);
+            },
+
+            delParent: function (child) {
+                child.parent = null;
+            },
+
+            //NO LA ENTIENDO
+            embody: function () {
+                if (this.stage && this.body) {
+                    this.stage.addChild(this.body);
+                    this.render();
+                }
+            },
+
+            gesture: function (name) {
+                if (!this.gestures[name]) {
+                    console.err('Entity', '::', 'gesture desconicida', name);
+                    return;
+                }
+
+                if (this.state.gesture == name) {
+                    return;
+                }
+
+                this.state.gesture = name;
+                var gestureDef = this.gestures[name];
+
+                if (!this.sprites[gestureDef.sprite]) {
+                    console.err('Entity', '::', 'no se ha definido el sprite:', gestureDef.sprite)
+                }
+
+                var sprite = this.sprites[gestureDef.sprite];
+
+                this.body.removeAllChildren();
+                this.body.addChild(sprite);
+
+                sprite.gotoAndPlay(gestureDef.animation);
+            },
+
+            act: function (action) {
+                console.log('Entity', '::', 'actuando', 'acción:', action);
+
+                var d = $.Deferred();
+
+                if (_.isFunction(this[action.name])) {
+                    var promise = this[action.name].apply(this, action.params);
+
+                    // Es una promesa?
+                    if (promise && _.isFunction(promise.promise)) {
+                        promise.then(
+                            function () {
+                                d.resolve();
+                            }, function () {
+                                d.resolve();
+                            }
+                        );
+                    } else {
+                        d.resolve();
+                    }
+                } else {
+                    console.err('Entity', '::', 'Acción desconocida: ', action.name);
+                    d.resolve();
+                }
+
+                return d.promise();
+            },
+
+            interact: function (target, action) {
+                console.log('Entity', '::', 'interactuando', 'acción:', action);
+                return target.act(action);
+            },
+
+            render: function () {
+                if (this.stage && this.body) {
+                    this.stage.update();
+                }
+            },
+
+            // Acciones
+            teleport: function (x, y) {
+                this.body.x = x;
+                this.body.y = y;
             }
-        },
-
-        render: function () {
-            if (this.stage && this.body) {
-                this.stage.update();
-            }
-        }
+        });
     });
-});
