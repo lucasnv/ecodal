7d7cdf7e6682de4a870e62b4d4f12ef7edff5ca3
Lucas Vazquez <lnvf@ovgsistemas.com>
2013-10-15 02:08:06 -0300

    se agrega page tips Se realizan cambios sobre el menú de navegación

--- src/Gma/SiteBundle/Controller/DefaultController.php
+++ src/Gma/SiteBundle/Controller/DefaultController.php
@@ -8,21 +8,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GmaSiteBundle:Default:index.html.twig', array());
+        return $this->render('GmaSiteBundle:Default:index.html.twig', array('pageHome'=>true));
     }
 
     public function academicAction()
     {
-        return $this->render('GmaSiteBundle:Default:academic.html.twig',array());
+        return $this->render('GmaSiteBundle:Default:academic.html.twig',array('pageAcademic'=>true));
     }
 
     public function contactAction()
     {
-        return $this->render('GmaSiteBundle:Default:contact.html.twig',array());
+        return $this->render('GmaSiteBundle:Default:contact.html.twig',array('pageContact'=>true));
     }
 
     public function projectAction()
     {
-        return $this->render('GmaSiteBundle:Default:project.html.twig',array());
+        return $this->render('GmaSiteBundle:Default:project.html.twig',array('pageProject'=>true));
+    }
+
+    public function tipsAction()
+    {
+        return $this->render('GmaSiteBundle:Default:tips.html.twig',array('pageTips'=>true));
     }
 }
