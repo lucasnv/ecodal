556d4ef2e9e418d384c057f817878b33cf1b4703
Diego Perez Barreiro <dperez@ebowe.com>
2013-10-16 10:22:35 -0300

    room fix

--- web/game/js/gma/home/Room.js
+++ web/game/js/gma/home/Room.js
@@ -16,15 +16,17 @@ define(['myclass', 'gma/Entity', 'gma/home/Power'], function (my, Entity, Power)
 
             if (value) {
                 this.power.energy = 1;
-                var matrix = new createjs.ColorMatrix().adjustHue(180).adjustSaturation(100);
-                var filter = new createjs.ColorMatrixFilter(matrix);
-                this.body.filters = [filter];
-                this.body.cache(this.body.x, this.body.y, this.body.width, this.body.height);
+                /*var matrix = new createjs.ColorMatrix().adjustHue(180).adjustSaturation(100);
+                 var filter = new createjs.ColorMatrixFilter(matrix);
+                 this.body.filters = [filter];
+                 this.body.cache(this.body.x, this.body.y, this.body.width, this.body.height);
+                 */
             } else {
                 this.power.energy = 0;
-                console.log('apago');
-                this.body.filters = [];
-                this.body.cache(this.body.x, this.body.y, this.body.width, this.body.height);
+                /*console.log('apago');
+                 this.body.filters = [];
+                 this.body.cache(this.body.x, this.body.y, this.body.width, this.body.height);
+                 */
             }
 
             this.render();
