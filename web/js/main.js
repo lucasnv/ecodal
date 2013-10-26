f4c750fde02e5c53378f06a84ae9da9ad374c05f
lvazquez <lvazquez@ebowe.com>
2013-10-26 02:14:14 -0300

    Se agrega audio Se edita index

---
+++ web/js/main.js
@@ -0,0 +1,40 @@
+/*var manifest = [
+    {src: "/audio/bienvenida.mp3", id: "au-bienvenida"}
+];
+
+var Resources = {
+    loader: null
+};
+
+
+        loader = new createjs.LoadQueue();
+        loader.installPlugin(createjs.Sound);
+        loader.addEventListener('complete', function () {
+            console.log('COMPLETO');
+            d.resolve();
+        });
+
+        if (manifest.length) {
+            loader.loadManifest(manifest);
+        }
+
+*
+
+ createjs.Sound.addEventListener("fileload", createjs.proxy(this.loadHandler, this));
+ createjs.Sound.registerSound("/audio/bienvenida.mp3", "au-bienvenida");
+
+ function loadHandler(event) {
+     // This is fired for each sound that is registered.
+     var instance = createjs.Sound.play("au-bienvenida");  // play using id.  Could also use full sourcepath or event.src.
+    // instance.addEventListener("complete", createjs.proxy(this.handleComplete, this));
+     instance.volume = 0.5;
+ }
+
+/*
+
+$(document).ready(function () {
+    $(window).resize(function () {
+
+    });
+});
+*/
